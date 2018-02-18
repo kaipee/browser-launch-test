c8f573f72a43e9c1581aa6af32d5688d1c356b8e
kaipee <kaipee@kaipee.co.uk>
2018-02-18 01:27:17 +0000

    first commit

---
+++ browser_test.sh
@@ -0,0 +1,14 @@
+# Launch both firefox and google-chrome
+(nohup firefox >/dev/null 2>&1 &) && (nohup google-chrome-stable >/dev/null 2>&1 &)
+
+# Wait for the Chrome window to appear
+status=0
+while [ $status -eq 0 ]
+do
+	sleep 1
+	status=`wmctrl -x -l | grep "Google Chrome" | wc -l | gawk '{print $1}'`
+done
+
+# Move to secondary monitor
+# wmctrl [resize] window with title [Window Title] [and move] [zindex,x,y,w,h]
+wmctrl -r 'Google Chrome' -e '0,1600,0,1600,900'
